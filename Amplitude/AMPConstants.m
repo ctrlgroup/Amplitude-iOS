701a90847ad2525d5fd8c889d0827bf2491c2c8c
Lily Hoskin <Lily418@users.noreply.github.com>
2017-11-09 11:53:46 +0000

    Update to pathway-proxy.ctrl-group.io

--- Amplitude/AMPConstants.m
+++ Amplitude/AMPConstants.m
@@ -5,8 +5,8 @@
 
 NSString *const kAMPLibrary = @"amplitude-ios";
 NSString *const kAMPVersion = @"4.0.1";
-NSString *const kAMPEventLogDomain = @"pathway-proxy.cognitionkit.com";
-NSString *const kAMPEventLogUrl = @"https://pathway-proxy.cognitionkit.com";
+NSString *const kAMPEventLogDomain = @"pathway-proxy.ctrl-group.io";
+NSString *const kAMPEventLogUrl = @"https://pathway-proxy.ctrl-group.io";
 NSString *const kAMPDefaultInstance = @"$default_instance";
 const int kAMPApiVersion = 3;
 const int kAMPDBVersion = 3;
